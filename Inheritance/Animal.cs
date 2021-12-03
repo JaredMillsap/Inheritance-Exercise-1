76ead4c62179ea735cb64a363df2b9882f92c115
JaredMillsap <jmillsap23@gmail.com>
2021-12-03 10:35:00 -0600

    new

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Animal
+    {
+      
+
+        public int Legs { get; set; }
+        public int Age { get; set; }
+
+        public string Owner { get; set; }
+
+        public string Size { get; set; }
+
+    }
+
+}
